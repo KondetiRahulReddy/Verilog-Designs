8848f8293dddab20bf42ba5ffc0c1e863227def8
KondetiRahulReddy <rahulreddykondeti@gmail.com>
2025-11-27 18:45:40 -0800

    Fix mod-10 counter: wrap at 9, use sized literals

--- mod_10_Counter/src/dut.v
+++ mod_10_Counter/src/dut.v
@@ -1,13 +1,14 @@
-// Code your design here
+
 module tenCounter(input clk, reset, output reg[3:0] out);
   always @ (posedge clk) begin
     if (reset == 0)
-      out <= 0;
+      out <= 4'd0;
     else begin
-      if (out == 10)
-        out <= 0;
-      out <= out + 1;
-    end 
-      
-  end 
+      // count 0..9 then wrap to 0
+      if (out == 4'd9)
+        out <= 4'd0;
+      else
+        out <= out + 4'd1;
+    end
+  end
 endmodule
--- mod_10_Counter/src/tb_dut.v
+++ mod_10_Counter/src/tb_dut.v
@@ -2,22 +2,23 @@
 module tb;
   reg clk;
   reg reset;
-  
+
   wire [3:0] out;
-  
-  tenCounter t10(clk,reset,out);
-  
+
+  tenCounter t10(clk, reset, out);
+
   always
-    #5 clk = ~ clk;
-  
-  initial
-  {clk, reset} = {0,0};
-  
+    #5 clk = ~clk;
+
   initial begin
+    // initialize signals at time 0 with sized values
+    clk   = 1'b0;
+    reset = 1'b0;
+
     $dumpfile("mod10_tb.vcd");
     $dumpvars(0, tb);
-    #10 reset <= 1;
+    #10 reset = 1'b1;
     #300 $finish;
-  end  
-  
+  end
+
 endmodule
